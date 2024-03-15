97709dea10a471c8a2f6488912711efc16aa0ae4
the-null-dev <ephantus640@gmail.com>
2024-03-15 13:50:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+ # create a python file called main.py and add the following code to it.
+
+from flask import Flask, render_template, request
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
